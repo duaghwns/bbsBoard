2fbe3063c8deb654bb469b364c80a338f5d072ca
duaghwns <hj2758@gmail.com>
2022-02-04 21:50:05 +0900

    Setting

---
+++ src/main/java/com/duaghwns/bbsBoard/BbsBoardApplication.java
@@ -0,0 +1,13 @@
+package com.duaghwns.bbsBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duaghwns/bbsBoard/BbsBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duaghwns.bbsBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
